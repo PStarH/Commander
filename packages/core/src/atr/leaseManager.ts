3c55dc4658ce02059c72349cf1c79d06ac31ce7f
PStarH <sampan090611@gmail.com>
2026-06-24 23:31:21 +0800

    refactor(atr): remove dead imports, unused constants, and unused args

--- packages/core/src/atr/leaseManager.ts
+++ packages/core/src/atr/leaseManager.ts
@@ -56,7 +56,6 @@ interface BetterSqlite3DB {
 
 let BetterSqlite3: { new (filePath: string): BetterSqlite3DB } | null = null;
 try {
-  // eslint-disable-next-line @typescript-eslint/no-require-imports
   BetterSqlite3 = require('better-sqlite3');
 } catch (_silentE_) {
   reportSilentFailure(_silentE_, 'leaseManager:60');
