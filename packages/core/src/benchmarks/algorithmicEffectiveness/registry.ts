8bc7412aacc206230eef0c3f48b51d269a121c65
PStarH <sampan090611@gmail.com>
2026-07-09 16:09:39 +0800

    fix(benchmarks): register all algorithmic effectiveness modules in registry

--- packages/core/src/benchmarks/algorithmicEffectiveness/registry.ts
+++ packages/core/src/benchmarks/algorithmicEffectiveness/registry.ts
@@ -1,5 +1,7 @@
 import type { BenchmarkModule } from './types';
 import { thompsonMemoryModule } from './modules/thompsonMemory';
+import { adaptiveStoppingModule } from './modules/adaptiveStopping';
+import { swarmOrchestratorModule } from './modules/swarmOrchestrator';
 import { dynamicCostGuardianModule } from './modules/dynamicCostGuardian';
 import { parameterControllerModule } from './modules/parameterController';
 import { strategySelectorModule } from './modules/strategySelector';
@@ -9,9 +11,14 @@ import { smartModelRouterModule } from './modules/smartModelRouter';
 import { effortScalerModule } from './modules/effortScaler';
 import { topologyRouterModule } from './modules/topologyRouter';
 import { tokenGovernorModule } from './modules/tokenGovernor';
+import { tokenSentinelModule } from './modules/tokenSentinel';
 import { executionRouterModule } from './modules/executionRouter';
+import { providerFallbackChainModule } from './modules/providerFallbackChain';
 import { llmRetryModule } from './modules/llmRetry';
 import { circuitBreakerModule } from './modules/circuitBreaker';
+import { bm25ToolDiscoveryModule } from './modules/bm25ToolDiscovery';
+import { speculativeExecutorModule } from './modules/speculativeExecutor';
+import { fusionEngineModule } from './modules/fusionEngine';
 import { costPredictorModule } from './modules/costPredictor';
 import { modelCascadeControllerModule } from './modules/modelCascadeController';
 
@@ -36,6 +43,8 @@ export function getAllModules(): BenchmarkModule[] {
 }
 
 registerModule(thompsonMemoryModule);
+registerModule(adaptiveStoppingModule);
+registerModule(swarmOrchestratorModule);
 registerModule(dynamicCostGuardianModule);
 registerModule(parameterControllerModule);
 registerModule(strategySelectorModule);
@@ -45,8 +54,13 @@ registerModule(smartModelRouterModule);
 registerModule(effortScalerModule);
 registerModule(topologyRouterModule);
 registerModule(tokenGovernorModule);
+registerModule(tokenSentinelModule);
 registerModule(executionRouterModule);
+registerModule(providerFallbackChainModule);
 registerModule(llmRetryModule);
 registerModule(circuitBreakerModule);
+registerModule(bm25ToolDiscoveryModule);
+registerModule(speculativeExecutorModule);
+registerModule(fusionEngineModule);
 registerModule(costPredictorModule);
 registerModule(modelCascadeControllerModule);
